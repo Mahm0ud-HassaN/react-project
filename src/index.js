2282d86d013c184cd111bf9facc7226e68ee2051
azzas7dawy <azza20saeed@gmail.com>
2025-02-08 23:10:15 +0200

    Saved local changes

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App.js';
+import reportWebVitals from './reportWebVitals.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
